3b6afcf28fa3a3144a4bf675cd3caf5d9a4c6106
echchoura mohammed amine <113258573+echchoura-mohammed-amine@users.noreply.github.com>
2025-05-08 17:32:37 +0100

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,8 @@ import { getFirestore } from 'firebase/firestore'; // Uncommented
 // Your web app's Firebase configuration
 // IMPORTANT: Replace with your actual Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDEr7yBrJd55XmGo_zTq35WoWb9t2NkceI",
-  authDomain: "taskzen-yafgm.firebaseapp.com",
-  projectId: "taskzen-yafgm",
-  storageBucket: "taskzen-yafgm.firebasestorage.app",
-  messagingSenderId: "200890839133",
-  appId: "1:200890839133:web:6e81340561af006d975db4"
+
+  
 };
 
 // Initialize Firebase
